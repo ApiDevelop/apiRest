b873d0b12c799c4e9f98098d6e46190f9eb739ea
ApiDevelop <project.api.git@gmail.com>
2020-09-06 17:17:30 -0300

    Swagger file creation and deploy on heroku plataform

--- src/main/java/com/example/apiRest/controller/FilmsController.java
+++ src/main/java/com/example/apiRest/controller/FilmsController.java
@@ -2,6 +2,8 @@ package com.example.apiRest.controller;
 
 import com.example.apiRest.model.Films;
 import com.example.apiRest.repository.FilmsRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,28 +11,43 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1")
+@Api(value = "API REST Films")
+@CrossOrigin(origins = "*")
 public class FilmsController {
 
     @Autowired
     FilmsRepository filmsRepository;
 
     @GetMapping("/films")
+    @ApiOperation(value = "Return a list of films")
     public List<Films> getAllFilms(){
         return filmsRepository.findAll();
     }
 
     @GetMapping("/films/{id}")
+    @ApiOperation(value = "Return just one film")
     public Films getOneFilms(@PathVariable(value = "id")long id){
         return filmsRepository.findById(id);
     }
 
     @PostMapping("/films")
+    @ApiOperation(value = "Include a new film")
     public Films registerOneFilms(@RequestBody Films film){
         return filmsRepository.save(film);
     }
 
     @DeleteMapping("/films")
+    @ApiOperation(value = "Delete one film")
     public void deleteOneFilms(@RequestBody Films film) {
         this.filmsRepository.delete(film);
     }
+
+    @PutMapping("/films")
+    @ApiOperation(value = "Update data of one film")
+    public Films updateFilm(@RequestBody Films film) {
+        return filmsRepository.save(film);
+    }
+
+
+
 }
