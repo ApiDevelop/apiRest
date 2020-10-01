e1cb7642cba6ec8e78516492a0db08ecd768d861
Rodrigo Barreto <rgarciabarreto@gmail.com>
2020-09-30 21:20:16 -0300

    Mandatory Fields

--- src/main/java/com/example/apiRest/dto/FilmsDTO.java
+++ src/main/java/com/example/apiRest/dto/FilmsDTO.java
@@ -1,12 +1,17 @@
 package com.example.apiRest.dto;
 
+
 import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 public class FilmsDTO implements Serializable {
@@ -19,12 +24,18 @@ public class FilmsDTO implements Serializable {
 
     @ApiModelProperty(value = "Field is string")
     @NotBlank(message = "Field name can not be blank")
+    @Length(min = 3,max = 50,message = "The field name must have between 3 and 50 character ")
     private String nome;
 
-    @NotNull
+    @ApiModelProperty(value = "Field is string")
+    @NotBlank(message = "Field genre can not be blank")
     private String genero;
 
+    @ApiModelProperty(value = "Field is Int")
+
     @Column(name = "ano_de_lancamento")
+    @NotNull(message = "Field year can not be null")
+    @Range(min = 1900, max = 2025, message = "Field year should be between 1900 and 2025")
     private int ano;
 
     public Long getId() {
@@ -59,4 +70,5 @@ public class FilmsDTO implements Serializable {
     public void setAno(int ano) {
         this.ano = ano;
     }
+
 }
